0d5a68cf78662c7316ca4630facfb28242316502
Michael Faes <rolve@trick17.ch>
2021-01-29 16:23:23 +0100

    Spring Boot project for Grading Service

---
+++ grading-service/src/main/java/ch/trick17/gradingserver/gradingservice/GradingServiceApplication.java
@@ -0,0 +1,13 @@
+package ch.trick17.gradingserver.gradingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GradingServiceApplication.class, args);
+    }
+
+}
---
+++ grading-service/src/test/java/ch/trick17/gradingserver/gradingservice/GradingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.trick17.gradingserver.gradingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
