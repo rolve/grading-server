d09f201f94fe3ebcf00578ac61755d94d26809ef
Michael Faes <rolve@trick17.ch>
2021-02-02 13:23:44 +0100

    Remove false JSON warnings

--- grading-service/src/main/java/ch/trick17/gradingserver/gradingservice/GradingServiceApplication.java
+++ grading-service/src/main/java/ch/trick17/gradingserver/gradingservice/GradingServiceApplication.java
@@ -3,7 +3,9 @@ package ch.trick17.gradingserver.gradingservice;
 import ch.trick17.jtt.grader.Grader;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateCustomizer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 
 @SpringBootApplication
 public class GradingServiceApplication {
@@ -19,5 +21,22 @@ public class GradingServiceApplication {
         grader.setResultsDir(null);
         grader.setParallelism(1); // no need for parallelism, grading one at a time
         return grader;
+    }
+
+    @Bean
+    RestTemplateCustomizer unwantedConvertersRemovingCustomizer() {
+        // see https://blog.trifork.com/2020/05/26/i-used-springs-resttemplate-to-fetch-some-json-and-you-wont-believe-what-happened-next/
+        return template -> {
+            var foundFirst = false;
+            for (var i = template.getMessageConverters().iterator(); i.hasNext();) {
+                if (i.next() instanceof MappingJackson2HttpMessageConverter) {
+                    if (foundFirst) {
+                        i.remove();
+                    } else {
+                        foundFirst = true;
+                    }
+                }
+            }
+        };
     }
 }
