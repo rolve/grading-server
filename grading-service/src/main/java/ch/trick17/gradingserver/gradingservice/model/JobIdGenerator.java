c90ff234db51e48de58a4de8da774e99e0d14d3a
Michael Faes <rolve@trick17.ch>
2021-02-04 17:25:00 +0100

    Move random string generation into separate class, to be reused in other places

--- grading-service/src/main/java/ch/trick17/gradingserver/gradingservice/model/JobIdGenerator.java
+++ grading-service/src/main/java/ch/trick17/gradingserver/gradingservice/model/JobIdGenerator.java
@@ -1,7 +1,9 @@
 package ch.trick17.gradingserver.gradingservice.model;
 
+import ch.trick17.gradingserver.RandomHexStringGenerator;
 import org.hibernate.engine.spi.SharedSessionContractImplementor;
 import org.hibernate.id.IdentifierGenerator;
+import org.hibernate.persister.entity.EntityPersister;
 
 import java.util.Random;
 
@@ -10,16 +12,12 @@ import static org.hibernate.LockMode.NONE;
 
 public class JobIdGenerator implements IdentifierGenerator {
 
+    // generate 128-bit hex strings, similar to Git commit hashes
+    private final RandomHexStringGenerator generator = new RandomHexStringGenerator(32);
+
     @Override
     public String generate(SharedSessionContractImplementor session, Object o) {
-        // generate 128-bit hex strings, similar to Git commit hashes
-        var random = new Random();
-        String id;
-        Object existing;
-        do {
-            id = format("%016x%016x", random.nextLong(), random.nextLong());
-            existing = session.getEntityPersister(GradingJob.class.getName(), o).load(id, null, NONE, session);
-        } while (existing != null);
-        return id;
+        var persister = session.getEntityPersister(GradingJob.class.getName(), o);
+        return generator.generate(id -> persister.load(id, null, NONE, session) != null);
     }
 }
