c9d42bf23a9aa110ec6e8582e8086965604dd1bf
Michael Faes <rolve@trick17.ch>
2021-02-17 17:50:39 +0100

    Secure this thing.

---
+++ web-app/src/main/java/ch/trick17/gradingserver/webapp/WebSecurityConfig.java
@@ -0,0 +1,29 @@
+package ch.trick17.gradingserver.webapp;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                    .regexMatchers("/",
+                            "/css/.*",
+                            "/courses/\\d+/",
+                            "/courses/\\d+/problem-sets/\\d+/").permitAll()
+                    .anyRequest().authenticated()
+                    .and()
+                .formLogin()
+                    .loginPage("/login")
+                    .permitAll()
+                    .and()
+                .logout()
+                    .permitAll();
+    }
+}
