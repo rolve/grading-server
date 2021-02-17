051aacf8da6a6af95482d0d0c891012df436165b
Michael Faes <rolve@trick17.ch>
2021-02-17 18:33:08 +0100

    Show modification buttons only when a user is logged in. Now with Thymeleaf security extras.

--- web-app/src/main/java/ch/trick17/gradingserver/webapp/model/CourseRepository.java
+++ web-app/src/main/java/ch/trick17/gradingserver/webapp/model/CourseRepository.java
@@ -1,6 +1,7 @@
 package ch.trick17.gradingserver.webapp.model;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
-public interface CourseRepository extends PagingAndSortingRepository<Course, Integer> {}
+public interface CourseRepository extends JpaRepository<Course, Integer> {}
