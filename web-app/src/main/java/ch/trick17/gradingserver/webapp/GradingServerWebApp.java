2de3eda236781b8bd6dc96c21e36955a1e10fbfa
Michael Faes <rolve@trick17.ch>
2021-02-23 19:55:36 +0100

    Pretty times

--- web-app/src/main/java/ch/trick17/gradingserver/webapp/GradingServerWebApp.java
+++ web-app/src/main/java/ch/trick17/gradingserver/webapp/GradingServerWebApp.java
@@ -1,14 +1,15 @@
 package ch.trick17.gradingserver.webapp;
 
+import org.ocpsoft.prettytime.PrettyTime;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.task.TaskExecutor;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
+import java.util.function.Supplier;
 
 @SpringBootApplication
 @EnableAsync
@@ -22,5 +23,10 @@ public class GradingServerWebApp {
     @Bean
     public Executor taskExecutor() {
         return Executors.newCachedThreadPool();
+    }
+
+    @Bean
+    public Supplier<PrettyTime> prettyTime() {
+        return PrettyTime::new;
     }
 }
