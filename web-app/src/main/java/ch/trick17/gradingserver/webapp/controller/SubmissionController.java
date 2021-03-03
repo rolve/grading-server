2c4bedf13625f1eb906fdcd97b710125167ab8d5
Michael Faes <rolve@trick17.ch>
2021-03-03 11:17:23 +0100

    A button for re-grading (mainly for debugging, I guess)

--- web-app/src/main/java/ch/trick17/gradingserver/webapp/controller/SubmissionController.java
+++ web-app/src/main/java/ch/trick17/gradingserver/webapp/controller/SubmissionController.java
@@ -2,10 +2,12 @@ package ch.trick17.gradingserver.webapp.controller;
 
 import ch.trick17.gradingserver.webapp.model.Submission;
 import ch.trick17.gradingserver.webapp.model.SubmissionRepository;
+import ch.trick17.gradingserver.webapp.service.GradingService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -18,9 +20,11 @@ public class SubmissionController {
     // TODO: grant access only to the authors of a submission
 
     private final SubmissionRepository repo;
+    private final GradingService gradingService;
 
-    public SubmissionController(SubmissionRepository repo) {
+    public SubmissionController(SubmissionRepository repo, GradingService gradingService) {
         this.repo = repo;
+        this.gradingService = gradingService;
     }
 
     @GetMapping("/{id}/")
@@ -30,6 +34,14 @@ public class SubmissionController {
         var submission = findSubmission(courseId, probId, solId, id);
         model.addAttribute("submission", submission);
         return "submissions/submission";
+    }
+
+    @PostMapping("/{id}/re-grade")
+    public String reGrade(@PathVariable int courseId, @PathVariable int probId,
+                          @PathVariable int solId, @PathVariable int id) {
+        var submission = findSubmission(courseId, probId, solId, id);
+        gradingService.grade(submission);
+        return "redirect:.";
     }
 
     private Submission findSubmission(int courseId, int probId, int solId, int id) {
