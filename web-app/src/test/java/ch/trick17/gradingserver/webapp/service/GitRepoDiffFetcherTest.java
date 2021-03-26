5dcfcd843370fd10e7f72131d1438b29860f2d3d
Michael Faes <rolve@trick17.ch>
2021-03-26 18:08:02 +0100

    Faster test by reusing fetcher

--- web-app/src/test/java/ch/trick17/gradingserver/webapp/service/GitRepoDiffFetcherTest.java
+++ web-app/src/test/java/ch/trick17/gradingserver/webapp/service/GitRepoDiffFetcherTest.java
@@ -1,21 +1,32 @@
 package ch.trick17.gradingserver.webapp.service;
 
 import org.eclipse.jgit.api.errors.GitAPIException;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 class GitRepoDiffFetcherTest {
 
     static final String REPO = "https://gitlab.com/rolves-private-group/some-subgroup/rolve.git";
     static final String TOKEN = "5jiBFYSUisc-xbpCyLAW";  // read-only token from dummy user
 
+    static GitRepoDiffFetcher fetcher;
+
+    static {
+        try {
+            fetcher = new GitRepoDiffFetcher(REPO, "", TOKEN);
+        } catch (GitAPIException e) {
+            throw new AssertionError(e);
+        }
+    }
+
     @Test
     void create() throws GitAPIException, IOException {
-        var fetcher = new GitRepoDiffFetcher(REPO, "", TOKEN);
         var paths = fetcher.affectedPaths(
                 "28ee26ac5c662e0be8e5afdfffacad07141e4487",
                 "e96481f61e8333245bd2d89cd943b75136782047");
@@ -24,7 +35,6 @@ class GitRepoDiffFetcherTest {
 
     @Test
     void delete() throws GitAPIException, IOException {
-        var fetcher = new GitRepoDiffFetcher(REPO, "", TOKEN);
         var paths = fetcher.affectedPaths(
                 "5dcf0093c3cf97d50bba5b316a77586d6eb7c3f3",
                 "f09d278dce2255cf38bf1b002b66aabb15a0ab8f");
@@ -33,7 +43,6 @@ class GitRepoDiffFetcherTest {
 
     @Test
     void modify() throws GitAPIException, IOException {
-        var fetcher = new GitRepoDiffFetcher(REPO, "", TOKEN);
         var paths = fetcher.affectedPaths(
                 "e876d3e1d934879958937574aba3fa25b57f5ab5",
                 "28ee26ac5c662e0be8e5afdfffacad07141e4487");
@@ -42,7 +51,6 @@ class GitRepoDiffFetcherTest {
 
     @Test
     void multipleCommits() throws GitAPIException, IOException {
-        var fetcher = new GitRepoDiffFetcher(REPO, "", TOKEN);
         var paths = fetcher.affectedPaths(
                 "28ee26ac5c662e0be8e5afdfffacad07141e4487",
                 "5dcf0093c3cf97d50bba5b316a77586d6eb7c3f3");
@@ -51,7 +59,6 @@ class GitRepoDiffFetcherTest {
 
     @Test
     void fromStart() throws GitAPIException, IOException {
-        var fetcher = new GitRepoDiffFetcher(REPO, "", TOKEN);
         var paths = fetcher.affectedPaths(
                 null,
                 "e876d3e1d934879958937574aba3fa25b57f5ab5");
@@ -61,5 +68,10 @@ class GitRepoDiffFetcherTest {
                 null,
                 "5dcf0093c3cf97d50bba5b316a77586d6eb7c3f3");
         assertEquals(Set.of("README.md", "foo/foo.txt", "bar/bar.txt"), paths);
+    }
+
+    @AfterAll
+    static void close() {
+        fetcher.close();
     }
 }
