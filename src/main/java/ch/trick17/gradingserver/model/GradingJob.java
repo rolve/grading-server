e6cbb4a59d74e3a4ad382a6220bda66692c040fc
Michael Faes <rolve@trick17.ch>
2024-03-02 11:14:04 +0100

    Eliminate historical split between GradingService and JobRunner

--- src/main/java/ch/trick17/gradingserver/model/GradingJob.java
+++
@@ -1,8 +0,0 @@
-package ch.trick17.gradingserver.model;
-
-public record GradingJob(CodeLocation submission,
-                         String username,
-                         String password,
-                         ProjectConfig projectConfig,
-                         GradingConfig gradingConfig) {
-}
--- src/main/java/ch/trick17/gradingserver/service/GradingService.java
+++ src/main/java/ch/trick17/gradingserver/service/GradingService.java
@@ -1,44 +1,71 @@
 package ch.trick17.gradingserver.service;
 
-import ch.trick17.gradingserver.model.GradingJob;
-import ch.trick17.gradingserver.model.Submission;
-import ch.trick17.gradingserver.model.SubmissionRepository;
+import ch.trick17.gradingserver.GradingServerProperties;
+import ch.trick17.gradingserver.model.*;
+import ch.trick17.jtt.grader.Grader;
+import ch.trick17.jtt.grader.Property;
+import ch.trick17.jtt.memcompile.Compiler;
+import ch.trick17.jtt.sandbox.Whitelist;
+import ch.trick17.jtt.testrunner.TestRunner;
+import ch.trick17.jtt.testsuitegrader.TestSuiteGrader;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
-import java.util.Set;
-import java.util.concurrent.ConcurrentHashMap;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Future;
 
-import static java.lang.Runtime.getRuntime;
-import static java.util.Collections.newSetFromMap;
+import static ch.trick17.gradingserver.model.GradingResult.formatTestMethods;
+import static java.nio.file.Files.createDirectories;
+import static java.nio.file.Files.write;
+import static java.util.Collections.emptyList;
 import static java.util.concurrent.CompletableFuture.completedFuture;
 import static java.util.concurrent.Executors.newFixedThreadPool;
+import static org.eclipse.jgit.util.FileUtils.*;
+import static org.slf4j.LoggerFactory.getLogger;
 
 @Service
 public class GradingService {
 
+    private static final Path JOBS_ROOT = Path.of("grading-jobs").toAbsolutePath();
+
+    private static final Logger logger = getLogger(GradingService.class);
+
     private final SubmissionRepository submissionRepo;
     private final SubmissionService submissionService;
-    private final JobRunner jobRunner;
+    private final CodeDownloader downloader;
+
+    private final Grader grader;
+    private final TestSuiteGrader testSuiteGrader;
     @Lazy
     @Autowired
     private GradingService proxy;
 
     public GradingService(SubmissionRepository submissionRepo,
                           @Lazy SubmissionService submissionService,
-                          JobRunner jobRunner) {
+                          CodeDownloader downloader, GradingServerProperties properties) {
         this.submissionRepo = submissionRepo;
         this.submissionService = submissionService;
-        this.jobRunner = jobRunner;
+        this.downloader = downloader;
+
+        var testRunner = new TestRunner(properties.getTestRunnerVmArgs());
+        grader = new Grader(testRunner);
+        testSuiteGrader = new TestSuiteGrader(testRunner);
     }
 
     @Bean
     Executor gradingExecutor() {
+        // TODO: Enable parallel grading again, after checking that the test runner is robust
+        //  enough. Then, need to make sure that the naming of the grading directories is unique or
+        //  prevent the same submission to be graded multiple times in parallel (which is useless
+        //  anyway). Or, even better: implement full in-memory checkout and grading.
         return newFixedThreadPool(1);
     }
 
@@ -47,7 +74,7 @@ public class GradingService {
         submission.clearResult();
         submission.setGradingStarted(false);
         submissionRepo.save(submission);
-        // initialize lazy dependencies collection
+        // initialize lazy dependencies collection in this thread
         var ignored = submission.getSolution().getProblemSet()
                 .getProjectConfig().getDependencies().size();
         // then call async method through proxy
@@ -56,19 +83,105 @@ public class GradingService {
 
     @Async("gradingExecutor")
     Future<Void> doGrade(Submission submission) {
-        var code = submission.getCodeLocation();
-        var token = submission.getSolution().getAccessToken();
-        var username = token == null ? null : "";
-        var password = token == null ? null : token.getToken();
-        var projectConfig = submission.getSolution().getProblemSet().getProjectConfig();
-        var gradingConfig = submission.getSolution().getProblemSet().getGradingConfig();
-        var job = new GradingJob(code, username, password, projectConfig, gradingConfig);
-
+        logger.info("Grading Submission {} ({})", submission.getId(), submission.getCodeLocation());
         submissionService.setGradingStarted(submission, true);
 
-        var result = jobRunner.run(job);
+        GradingResult result;
+        try {
+            result = tryGrade(submission);
+            logger.info("Finished grading Submission {}", submission.getId());
+        } catch (Throwable e) {
+            logger.error("Error while grading Submission " + submission.getId(), e);
+            result = new ErrorResult(e.getClass().getSimpleName() + ": " + e.getMessage());
+        }
+
         // set result in separate, @Transactional method:
         submissionService.setResult(submission, result);
         return completedFuture(null);
+    }
+
+    private GradingResult tryGrade(Submission submission) throws IOException {
+        var projectConfig = submission.getSolution().getProblemSet().getProjectConfig();
+
+        var codeDir = JOBS_ROOT.resolve(submission.getId() + "-code");
+        var depsDir = JOBS_ROOT.resolve(submission.getId() + "-deps");
+        try {
+            var token = submission.getSolution().getAccessToken();
+            var username = token == null ? null : "";
+            var password = token == null ? null : token.getToken();
+            downloader.downloadCode(submission.getCodeLocation(), codeDir, username, password);
+
+            var dependencies = writeDependencies(projectConfig.getDependencies(), depsDir);
+
+            // TODO: handle case when project root is missing
+            var srcDir = codeDir
+                    .resolve(projectConfig.getProjectRoot())
+                    .resolve(projectConfig.getStructure().srcDirPath);
+
+            var gradingConfig = submission.getSolution().getProblemSet().getGradingConfig();
+            if (gradingConfig instanceof ImplGradingConfig config) {
+                // TODO: Refactor Grader to be able to apply package filter
+                // TODO: Refactor Grader to not require a submission name
+                var implSubmission = new Grader.Submission("", srcDir);
+                var options = config.options();
+                var task = Grader.Task.fromString(config.testClass())
+                        .compiler(Compiler.valueOf(options.compiler().name()))
+                        .repetitions(options.repetitions())
+                        .timeouts(options.repTimeout(), options.testTimeout())
+                        .permittedCalls(options.permRestrictions()
+                                ? Whitelist.DEFAULT_WHITELIST_DEF
+                                : null)
+                        .dependencies(dependencies);
+
+                var result = grader.grade(task, implSubmission);
+                return convert(result);
+            } else if (gradingConfig instanceof TestSuiteGradingConfig config) {
+                var testDir = codeDir
+                        .resolve(projectConfig.getProjectRoot())
+                        .resolve(projectConfig.getStructure().testDirPath);
+                var testSuiteSubmission = TestSuiteGrader.Submission.loadFrom(testDir, projectConfig.getPackageFilter());
+
+                var testSuiteResult = testSuiteGrader.grade(config.task(), testSuiteSubmission, dependencies);
+                if (testSuiteResult.emptyTestSuite() || testSuiteResult.compilationFailed()) {
+                    return new TestSuiteGradingResult(testSuiteResult, null);
+                }
+
+                var task = new Grader.Task(testSuiteSubmission.testSuite(), emptyList())
+                        .dependencies(dependencies);
+                var implSubmission = new Grader.Submission("", srcDir);
+                var implResult = grader.grade(task, implSubmission);
+
+                return new TestSuiteGradingResult(testSuiteResult, convert(implResult));
+            } else {
+                throw new AssertionError("Unknown grading config type: " + gradingConfig);
+            }
+        } finally {
+            try {
+                delete(codeDir.toFile(), RECURSIVE | RETRY);
+                delete(depsDir.toFile(), RECURSIVE | RETRY);
+            } catch (IOException e) {
+                logger.warn("Could not delete " + codeDir, e);
+            }
+        }
+    }
+
+    private ImplGradingResult convert(Grader.Result result) {
+        var props = result.properties().stream()
+                .map(Property::prettyName)
+                .toList();
+        return new ImplGradingResult(props, formatTestMethods(result.passedTests(), result.allTests()),
+                formatTestMethods(result.failedTests(), result.allTests()));
+    }
+
+    private List<Path> writeDependencies(List<JarFile> dependencies, Path dir)
+            throws IOException {
+        var paths = new ArrayList<Path>();
+        createDirectories(dir);
+        for (var dep : dependencies) {
+            var path = dir.resolve(dep.getFilename());
+            write(path, dep.getContent());
+            paths.add(path);
+        }
+        return paths;
     }
 }
