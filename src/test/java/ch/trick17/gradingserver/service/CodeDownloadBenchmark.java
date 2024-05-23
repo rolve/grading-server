1effdf7f8c21e7005d66c0bd229f5aad14161cdd
Michael Faes <rolve@trick17.ch>
2024-05-23 08:20:24 +0200

    Refactor CodeDownloadBenchmark a little

--- src/test/java/ch/trick17/gradingserver/service/CodeDownloadBenchmark.java
+++ src/test/java/ch/trick17/gradingserver/service/CodeDownloadBenchmark.java
@@ -27,7 +27,7 @@ import static org.eclipse.jgit.util.FileUtils.*;
 public class CodeDownloadBenchmark {
 
     static final String REPO = "https://github.com/rolve/java-teaching-tools.git";
-    static final String COMMIT = "6493a0fe36f3739929a981ce1440111d0071e08e";
+    static final String COMMIT_HASH = "6493a0fe36f3739929a981ce1440111d0071e08e";
 
     @Benchmark
     public Map<String, byte[]> viaDisk() throws IOException, GitAPIException {
@@ -36,7 +36,7 @@ public class CodeDownloadBenchmark {
                 .setURI(REPO)
                 .setDirectory(dir.toFile());
         try (var git = clone.call()) {
-            git.checkout().setName(COMMIT).call();
+            git.checkout().setName(COMMIT_HASH).call();
         }
         var result = new HashMap<String, byte[]>();
         try (var walk = Files.walk(dir).filter(Files::isRegularFile)) {
@@ -58,25 +58,23 @@ public class CodeDownloadBenchmark {
             var git = new Git(repo);
             git.fetch()
                     .setRemote(REPO)
-                    .setRefSpecs(COMMIT)
+                    .setRefSpecs(COMMIT_HASH)
                     .call();
-            try (var revWalk = new RevWalk(repo)) {
-                var tree = revWalk.parseCommit(fromString(COMMIT)).getTree();
-                var treeWalk = new TreeWalk(repo);
-                treeWalk.addTree(tree);
-                treeWalk.setRecursive(true);
-                var result = new HashMap<String, byte[]>();
-                while (treeWalk.next()) {
-                    var objectId = treeWalk.getObjectId(0);
-                    var objectLoader = repo.open(objectId);
-                    result.put(treeWalk.getPathString(), objectLoader.getBytes());
-                }
-                return result;
+
+            var commit = new RevWalk(repo).parseCommit(fromString(COMMIT_HASH));
+            var treeWalk = new TreeWalk(repo);
+            treeWalk.addTree(commit.getTree());
+            treeWalk.setRecursive(true);
+            var result = new HashMap<String, byte[]>();
+            while (treeWalk.next()) {
+                var loader = repo.open(treeWalk.getObjectId(0));
+                result.put(treeWalk.getPathString(), loader.getBytes());
             }
+            return result;
         }
     }
 
     public static void main(String[] args) throws IOException {
-        org.openjdk.jmh.Main.main(new String[] {CodeDownloadBenchmark.class.getName()});
+        org.openjdk.jmh.Main.main(new String[]{CodeDownloadBenchmark.class.getName()});
     }
 }
