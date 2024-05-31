fbaf9c8c2d419a1cc14baeca9a17cf70743466fa
Michael Faes <rolve@trick17.ch>
2024-05-31 18:39:27 +0200

    Improve feedback by indicating partially implemented suggestions and the number of detected and total bugs in that context

--- src/main/java/ch/trick17/gradingserver/service/TestSuiteResultService.java
+++ src/main/java/ch/trick17/gradingserver/service/TestSuiteResultService.java
@@ -3,27 +3,58 @@ package ch.trick17.gradingserver.service;
 import ch.trick17.gradingserver.model.Submission;
 import ch.trick17.gradingserver.model.TestSuiteGradingConfig;
 import ch.trick17.gradingserver.model.TestSuiteGradingResult;
+import ch.trick17.jtt.testrunner.TestMethod;
+import ch.trick17.jtt.testsuitegrader.Mutation;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.util.*;
 
-import static java.util.stream.Collectors.toSet;
+import static java.util.Collections.emptySet;
 
 @Service
 public class TestSuiteResultService {
 
     private static final int SUGGESTIONS = 3;
 
-    public List<String> getSuggestions(Submission submission, TestSuiteGradingResult result) {
+    public List<Suggestion> getSuggestions(Submission submission, TestSuiteGradingResult result) {
         var config = (TestSuiteGradingConfig) submission.getSolution().getProblemSet().getGradingConfig();
-        var missingTests = result.testSuiteResult().mutantResults().stream()
-                .filter(r -> r.passed())
-                .flatMap(r -> r.mutation().killers().stream())
-                .collect(toSet());
+
+        var killedMutants = new HashMap<TestMethod, Set<Mutation>>();
+        var totalMutants = new HashMap<TestMethod, Set<Mutation>>();
+        for (var r : result.testSuiteResult().mutantResults()) {
+            for (var test : r.mutation().killers()) {
+                totalMutants.computeIfAbsent(test, t -> new HashSet<>())
+                        .add(r.mutation());
+                if (!r.passed()) {
+                    killedMutants.computeIfAbsent(test, t -> new HashSet<>())
+                            .add(r.mutation());
+                }
+            }
+        }
+
+        // consider only mutants that are not also killed by "weaker" tests
+        // (earlier in the map of test descriptions)
+        var tests = new ArrayList<>(config.task().refTestDescriptions().keySet());
+        for (int i = 0; i < tests.size(); i++) {
+            var killed = killedMutants.get(tests.get(i));
+            var total = totalMutants.get(tests.get(i));
+            for (int j = i + 1; j < tests.size(); j++) {
+                killedMutants.getOrDefault(tests.get(j), emptySet()).removeAll(killed);
+                totalMutants.get(tests.get(j)).removeAll(total);
+            }
+        }
+
         return config.task().refTestDescriptions().entrySet().stream()
-                .filter(e -> missingTests.contains(e.getKey()))
-                .map(e -> e.getValue())
+                .map(e -> new Suggestion(e.getValue(),
+                        killedMutants.getOrDefault(e.getKey(), emptySet()).size(),
+                        totalMutants.get(e.getKey()).size()))
+                .filter(s -> s.mutantsKilled() < s.mutantsTotal())
                 .limit(SUGGESTIONS)
                 .toList();
     }
+
+    public record Suggestion(
+            String description,
+            int mutantsKilled,
+            int mutantsTotal) {}
 }
