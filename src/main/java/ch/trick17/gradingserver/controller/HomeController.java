67df5a8553d630b0905025b958cb4c98560dc432
Michael Faes <rolve@trick17.ch>
2024-10-19 18:21:55 +0200

    Remove unused parameter

--- src/main/java/ch/trick17/gradingserver/controller/HomeController.java
+++ src/main/java/ch/trick17/gradingserver/controller/HomeController.java
@@ -1,9 +1,7 @@
 package ch.trick17.gradingserver.controller;
 
 import ch.trick17.gradingserver.model.CourseRepository;
-import ch.trick17.gradingserver.model.User;
 import ch.trick17.gradingserver.service.AccessController;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,7 +20,7 @@ public class HomeController {
     }
 
     @GetMapping("/")
-    public String home(Model model, @AuthenticationPrincipal User user) {
+    public String home(Model model) {
         var allCourses = courseRepo.findAll(
                 by("term.year", "term.kind").descending().and(
                 by("name", "qualifier").ascending()));
