22c8f0fa6435bfbd072fbedbea54b532d2f96272
Michael Faes <rolve@trick17.ch>
2024-10-23 17:28:30 +0200

    Test course access rules and visible problem sets for lecturers

--- src/test/java/ch/trick17/gradingserver/controller/CourseControllerTest.java
+++ src/test/java/ch/trick17/gradingserver/controller/CourseControllerTest.java
@@ -19,8 +19,10 @@ import static ch.trick17.gradingserver.model.GradingOptions.Compiler.JAVAC;
 import static ch.trick17.gradingserver.model.ProblemSet.DisplaySetting.HIDDEN;
 import static ch.trick17.gradingserver.model.ProblemSet.DisplaySetting.WITH_SHORTENED_NAMES;
 import static ch.trick17.gradingserver.model.ProjectConfig.ProjectStructure.ECLIPSE;
+import static ch.trick17.gradingserver.model.Role.LECTURER;
 import static java.util.Collections.emptyList;
 import static org.hamcrest.Matchers.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -31,7 +33,9 @@ import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppC
 class CourseControllerTest {
 
     @Autowired
-    private CourseRepository repo;
+    CourseRepository repo;
+    @Autowired
+    UserRepository userRepo;
 
     MockMvc mockMvc;
 
@@ -45,7 +49,9 @@ class CourseControllerTest {
     @Test
     @DirtiesContext
     void coursePage() throws Exception {
+        var lecturer = userRepo.save(new User("lecturer", "password", "Lecturer", LECTURER));
         var course = new Course("OOPI2", new Term(2021, "FS"), "2Ibb1");
+        course.getLecturers().add(lecturer);
 
         var projectConfig = new ProjectConfig("", ECLIPSE, null, emptyList());
         var gradingConfig = new ImplGradingConfig("class Foo {}",
@@ -63,7 +69,31 @@ class CourseControllerTest {
 
         repo.save(course);
 
+        // anonymous users should not see hidden problem sets
         mockMvc.perform(get("/courses/" + course.getId() + "/"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("<title>OOPI2 (FS 2021 2Ibb1)</title>")))
+                .andExpect(content().string(containsString("Regular Problem Set")))
+                .andExpect(content().string(containsString("2021")))
+                .andExpect(content().string(not(containsString("Hidden Problem Set"))))
+                .andExpect(content().string(not(containsString("2022"))));
+
+        // lecturers of the course should see them
+        mockMvc.perform(get("/courses/" + course.getId() + "/").with(user(lecturer)))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("<title>OOPI2 (FS 2021 2Ibb1)</title>")))
+                .andExpect(content().string(containsString("Regular Problem Set")))
+                .andExpect(content().string(containsString("2021")))
+                .andExpect(content().string(containsString("Hidden Problem Set")))
+                .andExpect(content().string(containsString("2022")));
+
+        // other lecturers should not see them
+        var otherLecturer = userRepo.save(new User("other", "password", "Other", LECTURER));
+        var otherCourse = new Course("Other", new Term(2021, "FS"), "2Ibb1");
+        otherCourse.getLecturers().add(otherLecturer);
+        repo.save(otherCourse);
+
+        mockMvc.perform(get("/courses/" + course.getId() + "/").with(user(otherLecturer)))
                 .andExpect(status().isOk())
                 .andExpect(content().string(containsString("<title>OOPI2 (FS 2021 2Ibb1)</title>")))
                 .andExpect(content().string(containsString("Regular Problem Set")))
@@ -75,13 +105,31 @@ class CourseControllerTest {
     @Test
     @DirtiesContext
     void hiddenCoursePage() throws Exception {
+        var lecturer = userRepo.save(new User("lecturer", "password", "Lecturer", LECTURER));
         var course = new Course("OOPI2", new Term(2021, "FS"), "2Ibb1");
+        course.getLecturers().add(lecturer);
         course.setHidden(true);
         repo.save(course);
 
+        // anonymous users should not see hidden courses
         mockMvc.perform(get("/courses/" + course.getId() + "/"))
                 .andExpect(status().is3xxRedirection())
                 .andExpect(header().string("Location", containsString("/login")))
                 .andExpect(content().string(is(emptyString())));
+
+        // lecturers of a course should see it, of course
+        mockMvc.perform(get("/courses/" + course.getId() + "/").with(user(lecturer)))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("<title>OOPI2 (FS 2021 2Ibb1)</title>")));
+
+        var otherLecturer = userRepo.save(new User("other", "password", "Other", LECTURER));
+        var otherCourse = new Course("Other", new Term(2021, "FS"), "2Ibb1");
+        otherCourse.getLecturers().add(otherLecturer);
+        repo.save(otherCourse);
+
+        // other lecturers should not see it
+        mockMvc.perform(get("/courses/" + course.getId() + "/").with(user(otherLecturer)))
+                .andExpect(status().isForbidden())
+                .andExpect(content().string(not(containsString("OOPI2"))));
     }
 }
